8cb188c0d3fe0d3ca8e09912cbed5955e9d7a06e
pradeep-nair <pradeep.nair@appdynamics.com>
2018-08-13 15:57:55 -0700

    initial commit

---
+++ src/main/java/com/appdynamics/extensions/aws/kinesis/videostreams/util/Constants.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2018 AppDynamics,Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.appdynamics.extensions.aws.kinesis.videostreams.util;
+
+/**
+ * Created by pradeep.nair on 8/7/18.
+ * Stores defaults that are used throughout the project
+ */
+public class Constants {
+    public static final String DEFAULT_METRIC_PREFIX = "Custom Metrics|AWS Kinesis Video Streams|";
+    public static final String MONITOR_NAME = "KinesisVideoStreamsMonitor";
+}
